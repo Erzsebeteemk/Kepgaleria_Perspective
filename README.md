e879b686dd993f0c2a16188d68f5151094160fce
Erzsebeteemk <88439357+Erzsebeteemk@users.noreply.github.com>
2021-11-02 16:31:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kepgaleria_Perspective
+ 
---
+++ data.js
@@ -0,0 +1,116 @@
+let data0 = {
+    photo: "images/Sunrise.webp",
+    title: "Sunrise",
+    description: "Sunrise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data1 = {
+    photo: "images/WaterSkyRainbow.png",
+    title: "WaterSkyRainbow",
+    description: "WaterSkyRainbow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data2 = {
+    photo: "images/UnityWithWholeness.png",
+    title: "UnityWithWholeness Photo by Zoltan Turcsanyi",
+    description: "UnityWithWholeness Photo by Zoltan Turcsanyi   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data3 = {
+    photo: "images/Truth.jpg",
+    title: "Truth",
+    description: "Truth Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data4 = {
+    photo: "images/YinYangBalanceFlow.jpg",
+    title: "YinYangBalanceFlow",
+    description: "YinYangBalanceFlow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data5 = {
+    photo: "images/FromLove.jpg",
+    title: "FromLove",
+    description: "FromLove Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data6 = {
+    photo: "images/Alpen.JPG",
+    title: "Alpen",
+    description: "Alpen Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data7 = {
+    photo: "images/ArtOfNature.jpg",
+    title: "ArtOfNature",
+    description: "ArtOfNature Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data8 = {
+    photo: "images/ITWorldBSE.jpg",
+    title: "ITWorldBSE",
+    description: "ITWorldBSE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data9 = {
+    photo: "images/Fruit.png",
+    title: "Fruit",
+    description: "Fruit Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data10 = {
+    photo: "images/Energy.jpg",
+    title: "Energy",
+    description: "Energy Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data11 = {
+    photo: "images/VitalLife.jpg",
+    title: "VitalLife",
+    description: "VitalLife Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data12 = {
+    photo: "images/Universe.jpg",
+    title: "Universe",
+    description: "Universe Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data13 = {
+    photo: "images/Pureness.JPG",
+    title: "Pureness",
+    description: "Pureness Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data14 = {
+    photo: "images/Sunset.jpg",
+    title: "Sunset",
+    description: "Sunset Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+let data15 = {
+    photo: "images/MoonAndVenus.jpg",
+    title: "MoonAndVenus",
+    description: "MoonAndVenus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci non leo faucibus vulputate in sit amet nibh. Duis pretium, ligula eget ullamcorper aliquet, lectus elit pulvinar dolor, sed auctor massa erat sed metus."
+};
+
+
+
+let kepadatoktomb = [
+    data0,
+    data1,
+    data2,
+    data3,
+    data4,
+    data5,
+    data6,
+    data7,
+    data8,
+    data9,
+    data10,
+    data11,
+    data12,
+    data13,
+    data14,
+    data15
+];
